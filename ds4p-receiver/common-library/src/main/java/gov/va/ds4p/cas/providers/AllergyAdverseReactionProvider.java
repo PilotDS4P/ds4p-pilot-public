9b22bda464f42e3357753634ad2fb07b83ebf5f0
Duane DeCouteau <Duane DeCouteau@Socratic5>
2012-10-17 12:29:59 -0600

    initial commit ds4p receiver commons lib module

---
+++ ds4p-receiver/common-library/src/main/java/gov/va/ds4p/cas/providers/AllergyAdverseReactionProvider.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.va.ds4p.cas.providers;
+
+/**
+ *
+ * @author Duane DeCouteau
+ */
+public class AllergyAdverseReactionProvider {
+    
+}
