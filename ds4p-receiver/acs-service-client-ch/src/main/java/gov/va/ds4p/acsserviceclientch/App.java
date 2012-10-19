4849060e401f58bbf373579f6c8438df865ae000
Duane DeCouteau <Duane DeCouteau@Socratic5>
2012-10-19 13:27:48 -0600

    Sender Context Handler

---
+++ ds4p-receiver/acs-service-client-ch/src/main/java/gov/va/ds4p/acsserviceclientch/App.java
@@ -0,0 +1,13 @@
+package gov.va.ds4p.acsserviceclientch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
